7ec0b13cf9acac06b166541a1b2f93cf5c6d0e3b
EringiShimeji <59910028+EringiShimeji@users.noreply.github.com>
2023-08-02 09:59:46 +0000

    setup routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/top'
   devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :create, :show, :edit, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
